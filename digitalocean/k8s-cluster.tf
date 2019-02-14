99b9b3f3d21bdfd96f1196156e1542b344cdb87f
Jesse Adametz <jesseadametz@gmail.com>
2019-02-13 21:05:35 -0800

    Update K8s cluster version

--- digitalocean/k8s-cluster.tf
+++ digitalocean/k8s-cluster.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "k8s" {
     name    = "c5r"
     region  = "sfo2"
-    version = "1.13.1-do.2"
+    version = "1.13.1-do.3"
 
     node_pool {
         name        = "workers"

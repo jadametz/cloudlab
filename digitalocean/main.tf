eca8d405c7218a37ed2f1c1901138935167090e0
Jesse Adametz <jesseadametz@gmail.com>
2019-05-24 21:50:18 -0700

    Add terraform remote backend

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -4,3 +4,14 @@ provider "digitalocean" {
     token   = "${var.do_token}"
     version = "v1.1.0"
 }
+
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "jadametz"
+
+        workspaces {
+            name = "do-cloudlab"
+        }
+    }
+}

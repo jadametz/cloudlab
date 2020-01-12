2d726e3117c4c07d992c2f376637271acade8cb4
Jesse Adametz <jesseadametz@gmail.com>
2020-01-12 15:20:13 -0800

    K8s version 1.13.12-do.f.2

--- digitalocean/k8s-cluster.tf
+++ digitalocean/k8s-cluster.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "k8s" {
     name    = "c5r"
     region  = "sfo2"
-    version = "1.13.5-do.f.0"
+    version = "1.13.12-do.f.2"
 
     node_pool {
         name        = "workers"

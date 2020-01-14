5978e96f47dc1687a93edc1d9b78a3ca2a269b08
Jesse Adametz <jesseadametz@gmail.com>
2020-01-14 13:15:34 -0800

    Upgrade K8s version and vertically scale node pool

--- digitalocean/k8s-cluster.tf
+++ digitalocean/k8s-cluster.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "k8s" {
     name    = "c5r"
     region  = "sfo2"
-    version = "1.13.12-do.f.2"
+    version = "1.16.2-do.2"
 
     node_pool {
         name        = "workers"
-        size        = "s-1vcpu-2gb"
+        size        = "s-2vcpu-4gb"
         node_count  = 1
     }
 }

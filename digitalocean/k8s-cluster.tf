3c0092f1ffe5d74e45388a816b2661bf032a8b31
Jesse Adametz <jesseadametz@gmail.com>
2020-02-25 22:04:59 -0800

    scaling down the node size

--- digitalocean/k8s-cluster.tf
+++ digitalocean/k8s-cluster.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "k8s" {
     name    = "c5r"
     region  = "sfo2"
-    version = "1.16.2-do.2"
+    version = "1.16.6-do.0"
 
     node_pool {
         name        = "workers"
-        size        = "s-2vcpu-4gb"
+        size        = "s-1vcpu-2gb"
         node_count  = 1
     }
 }
